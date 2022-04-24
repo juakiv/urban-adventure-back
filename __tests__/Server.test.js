9bf270cf324379d6a1eaeef1a348eacf0d5f5d1f
Jussi <juakiv@utu.fi>
2022-04-24 13:00:39 +0300

    lisää testi

--- __tests__/Server.test.js
+++ __tests__/Server.test.js
@@ -1,12 +1,34 @@
 const WebSocketServer = require("../src/WebSocketServer");
 const http = require("http");
 const express = require("express");
+const WebSocket = require("ws");
+
+// odottaa websocket-yhteyden tilan muutosta
+function webSocketState(socket, state) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      if (socket.readyState === state) resolve();
+      else webSocketState(socket, state).then(resolve);
+    }, 100);
+  });
+}
 
 describe("server tests", () => {
   let server, wsServer;
-  beforeAll(() => {
+  const PORT = process.env.PORT || 3002;
+
+  // käynnistetään websocket-palvelin ennen jokaista testiä
+  beforeEach(() => {
     server = http.createServer(express());
+    server.listen(PORT);
+
     wsServer = new WebSocketServer(server);
+  });
+
+
+  // suljetaan websocket-palvelin testien jälkeen
+  afterEach(() => {
+    server.close(PORT);
   });
 
   test("it handles invalid JSON", () => {
@@ -15,4 +37,23 @@ describe("server tests", () => {
     expect(wsServer.checkMessageValidity(JSON.stringify({type: null}))).toBeFalsy();
     expect(wsServer.checkMessageValidity(JSON.stringify({type: "scores"}))).toBeTruthy();
   });
+
+  test("it responds with pong to a ping", async () => {
+    let client = new WebSocket(`ws://localhost:${PORT}`);
+    await webSocketState(client, client.OPEN);
+
+    let socketResponse;
+    client.on("message", message => {
+      socketResponse = JSON.parse(message);
+      
+      client.close();
+    });
+
+    client.send(JSON.stringify({ type: "ping" }));
+    
+    
+    await webSocketState(client, client.CLOSED);
+    expect(socketResponse).toStrictEqual({messageType: "pong"});
+  });
+
 });
